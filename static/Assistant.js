8bd349fbccdb98549e7cbfd9bf41e52481c10878
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-01-25 20:04:02 +0530

    Assistant updated

--- static/Assistant.js
+++ static/Assistant.js
@@ -429,7 +429,7 @@ class KwikChatAccessibility {
             }, 500);
         }
     }
-    
+
 
    speak(text, options = {}) {
     if (!this.isActive && !options.force) return;
@@ -439,7 +439,6 @@ class KwikChatAccessibility {
         
         const utterance = new SpeechSynthesisUtterance(text);
         
-        // Female voice 
         utterance.rate = 1.0;  
         utterance.pitch = 1.2;    
         utterance.volume = 0.9;   
