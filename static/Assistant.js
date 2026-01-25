c79093343e62c91e3b3baca4ae0292e2d23c26a5
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-01-25 20:09:41 +0530

    Assistant updated

--- static/Assistant.js
+++ static/Assistant.js
@@ -340,6 +340,7 @@ class KwikChatAccessibility {
             return;
         }
         
+        
         // Get last 3 messages
         const lastMessages = [];
         const startIndex = Math.max(0, messageElements.length - 3);
